ab158b9d45339ef080168cf5033f49bbfe30e2ee
Wera Barinova <alisawera@yandex.ru>
2025-06-06 18:07:59 +0300

    Вход на сайт и выход с сайта

--- blog/mainpage/urls.py
+++ blog/mainpage/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='mainpage'),
+    path(
+        'login/',                               # путь в браузере
+        auth_views.LoginView.as_view(           # django login
+            template_name='user/login.html'),   # шаблон
+        name='login'),                          # {% url name %}
+    path('logout/',
+         auth_views.LogoutView.as_view(
+         next_page='/'),
+         name='logout'),
 ]
